079de73f54ba12399bb5122ac5e78d92ce149827
markhorsman <mark.horsman@tam-one.nl>
2018-02-10 21:51:35 +0100

    Updated jdj-cert-worker process
    Updating stock item
    Creating contdoc record
    Copy file to location

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const each 					= require('lodash/each');
 const pdfText       = require('pdf-text');
 const moment        = require('moment');
 const nconf         = require('nconf');
+const fsPath        = require('fs-path');
+
 
 const pending       = [];
 const cnf           = nconf.argv().env().file({ file: path.resolve(__dirname + '/config.json') });
@@ -67,7 +69,20 @@ function initWatcher() {
           pending.push(path);
 
           genStockItemFromPDF(path)
+            .then(findContdocItem)
+            .then(obj => {
+                if (obj.resp.body) {
+                  const m = moment(obj.resp.body.SID);
+                  if (m.isValid() && moment(m.format('YYYY-MM-DD')).isSameOrAfter(moment(obj.stockItem.lastser).format('YYYY-MM-DD')))
+                    throw new Error('Certificate file ' + path + ' already processed');
+                }
+
+                return obj.stockItem;
+            })
             .then(findStockItem)
+            .then(updateStockItem)
+            .then(copyPDFToFolder)
+            .then(createContdoc)
             .then(result => {
               watcher.unwatch(path);
 
@@ -86,6 +101,25 @@ function initWatcher() {
   return watcher;
 }
 
+function copyPDFToFolder(obj)
+{
+    const filePath = cnf.get('pdfDir') + obj.stockItem.PGROUP + '/' + obj.stockItem.GRPCODE;
+    obj.pdfData.filename = filePath + '/' + obj.stockItem.ITEMNO + '.pdf';
+
+    return new bb((resolve, reject) => {
+      fsPath.mkdir(filePath, function(err) {
+        if (err) return reject(err);
+
+        fsPath.copy(obj.pdfData.path, obj.pdfData.filename, function(err) {
+          if (err) return reject(err);
+
+          return resolve(obj);
+        });
+      });
+
+    });
+}
+
 function findStockItem(stockItem)
 {
   return rp(
@@ -97,6 +131,69 @@ function findStockItem(stockItem)
       json: true
     }
   ).then(resp => ({ stockItem: resp.body, pdfData: stockItem }));
+}
+
+function findContdocItem(stockItem)
+{
+  return rp(
+    {
+      uri: cnf.get('api:uri') + '/contdoc/find/' + stockItem.itemno,
+      headers: {
+        'Authorization': 'Basic ' + new Buffer(cnf.get('api:auth:username') + ':' + cnf.get('api:auth:password')).toString('base64')
+      },
+      json: true
+    }
+  ).then(resp => ({ resp, stockItem }));
+}
+
+function updateStockItem(obj)
+{
+  return rp(
+    {
+      method: 'PUT',
+      uri: cnf.get('api:uri') + '/stock',
+      headers: {
+        'Authorization': 'Basic ' + new Buffer(cnf.get('api:auth:username') + ':' + cnf.get('api:auth:password')).toString('base64')
+      },
+      body: {
+        lastser: obj.pdfData.lastser,
+        period: 365,
+        serno: obj.pdfData.serno,
+        status: (parseInt(obj.stockItem.STATUS) === 11 ? 0 : obj.stockItem.STATUS),
+        pattest: 1,
+        patperiod: 365,
+        patlastser: obj.pdfData.lastser,
+        patpertype: 1,
+        itemno: obj.pdfData.itemno
+      },
+      json: true
+    }
+  ).then(resp => ({ stockItem: obj.stockItem, pdfData: obj.pdfData, resp }));
+}
+
+function createContdoc(obj)
+{
+  return rp(
+    {
+      method: 'POST',
+      uri: cnf.get('api:uri') + '/contdoc',
+      headers: {
+        'Authorization': 'Basic ' + new Buffer(cnf.get('api:auth:username') + ':' + cnf.get('api:auth:password')).toString('base64')
+      },
+      body: {
+        type: 'ST',
+        key: obj.pdfData.itemno,
+        filename: obj.pdfData.filename,
+        optflag: 0,
+        options: 0,
+        sid: obj.pdfData.lastser,
+        scantopdftype: 0,
+        name: 'Certificaat ' + moment().format('YYYY'),
+        showinweb: 0
+      },
+      json: true
+    }
+  ).then(resp => ({ stockItem: obj.stockItem, pdfData: obj.pdfData, resp }));
 }
 
 function genStockItemFromPDF(path) {
@@ -112,7 +209,7 @@ function genStockItemFromPDF(path) {
           stockItem.itemno  = articleNumber;
           stockItem.serno   = extractSerialNumber(chunks);
           stockItem.lastser = extractDate(chunks);
-
+          stockItem.path    = path;
           resolve(stockItem)
       })
       .catch(e => reject(e));
@@ -166,26 +263,27 @@ function extractDate(chunks) {
   const datumIndex              = chunks.indexOf('Datum:');
   const dateOfCalibrationIndex  = chunks.indexOf('Date of calibration');
   const dateOf1stTestIndex      = chunks.indexOf('(Date of 1st test)');
+  const time                    = moment().format('HH:mm:ss.SSS');
 
   if (datumIndex > -1) {
     const m = moment(chunks[datumIndex + 1], 'DD-MM-YYYY');
 
-    if (m.isValid()) return m.format('YYYY-MM-DD');
+    if (m.isValid()) return m.format('YYYY-MM-DD') + ' ' + time;
   }
 
   if (dateOfCalibrationIndex > -1) {
      const m = moment(chunks[dateOfCalibrationIndex + 2], 'DD MMMM YYYY');
 
-     if (m.isValid()) return m.format('YYYY-MM-DD')
+     if (m.isValid()) return m.format('YYYY-MM-DD') + ' ' + time;
   }
 
   if (dateOf1stTestIndex > -1) {
     const m = moment(chunks[dateOf1stTestIndex + 1], 'DD-MM-YYYY');
 
-    if (m.isValid()) return m.format('YYYY-MM-DD')
+    if (m.isValid()) return m.format('YYYY-MM-DD') + ' ' + time;
   }
 
-  return null;
+  return moment().format('YYYY-MM-DD HH:mm:ss');
 }
 
 function extractSerialNumber(chunks)
